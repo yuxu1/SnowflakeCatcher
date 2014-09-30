1668b20ca54f6d95dfb268ea99dfc0231e27c243
yuxu1 <yufanx8@gmail.com>
2014-09-30 11:03:16 -0700

    Added title and instructions to index.html and code to SnowflakeCatcher.pde

--- build-tmp/source/SnowflakeCatcher.java
+++ build-tmp/source/SnowflakeCatcher.java
@@ -1,7 +1,23 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class SnowflakeCatcher extends PApplet {
+
 Snowflake [] storm;
 Catcher bar=new Catcher();
 
-void setup()
+public void setup()
 {
   size (500, 500);
   background(0);
@@ -13,7 +29,7 @@ void setup()
   }
 }
 
-void draw()
+public void draw()
 {
   for (int i=0; i<storm.length; i++)
   {
@@ -38,12 +54,12 @@ class Snowflake
     y=(int)(Math.random()*500);
     isMoving=true;
   }
-  void erase()
+  public void erase()
   {
     fill(0);
     ellipse(x, y, 12, 12);
   }
-  void lookDown()
+  public void lookDown()
   {
     if (y>0 && y<500 && ((get(x+6, y+6))!=color(0) || (get(x-6, y+6))!=color(0)))
     {
@@ -53,14 +69,14 @@ class Snowflake
       isMoving=true;
     }
   }
-  void move()
+  public void move()
   {
     if (isMoving==true)
     {
       y=y+(int)(Math.random()*3+1);
     }
   }
-  void wrap()
+  public void wrap()
   {
     if ((y+6)>=500)
     {
@@ -68,7 +84,7 @@ class Snowflake
       x=(int)(Math.random()*500);
     }
   }
-  void show()
+  public void show()
   {
     fill(255);
     ellipse(x, y, 10, 10);
@@ -82,20 +98,20 @@ class Catcher
   {
     x2=(int)(Math.random()*500);
   }
-  void erase2()
+  public void erase2()
   {
     noStroke();
     fill(0);
     rect(x2,400,85,25);
   }
-  void show2()
+  public void show2()
   {
     noStroke();
     fill((int)(Math.random()*500), (int)(Math.random()*500), (int)(Math.random()*500));
     rect(x2, 400, 80, 20);
 
   }
-  void keyPressed()
+  public void keyPressed()
   {
     if (key=='a')
     {
@@ -116,3 +132,12 @@ class Catcher
   }
 }
 
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "SnowflakeCatcher" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
